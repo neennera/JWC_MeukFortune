c674c994ca8623de474682e6cc29fd8bdd863ac0
neennera <neennera@gmail.com>
2022-06-17 20:09:24 +0700

    add app

--- src/App.js
+++ src/App.js
@@ -24,21 +24,20 @@ function App2() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="nav-bar">
+        <nav>
+            <ul class="nav nav-pills justify-content-end py-3">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/home">เกี่ยวกับหมอหมึก</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/blog">บทความ</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/queue">จองคิว</a>
+                </li>
+            </ul>
+        </nav>
     </div>
   );
 } 
