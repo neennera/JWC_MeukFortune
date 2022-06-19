f84de5e02c9bac46e9d91f2acd45afe24462d564
neennera <neennera@gmail.com>
2022-06-19 23:09:52 +0700

    set html to react

---
+++ src/light.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+function light() {
+    return (
+
+        <>
+        <div class="stars"></div>
+        <div class="twinkling"></div> 
+        <div class="title">
+            <h1>A Dark and Mysterious Night</h1>
+        </div>
+       
+        </>
+    )
+}
+
+export default light;
