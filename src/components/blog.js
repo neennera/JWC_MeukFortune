d6cd943748a07973653931f5b3ba9ef651bd913a
neennera <neennera@gmail.com>
2022-06-24 22:57:18 +0700

    touch up

--- src/components/blog.js
+++ src/components/blog.js
@@ -12,46 +12,49 @@ function Infom(){
         <div class="row"><h2 class="py-2">อ่านบทความของหมอหมึก</h2></div>
         <div class="row py-2">
             <div class="column-3 align-items-center py-3">
-                <div classs="row">
+                <div classs="row SetHeight align-items-center">
                     <img src={Mathew} alt="Mathew" className="roundImage"/><br/><br/>
                     <h4>แมทธิว นักธุรกิจ</h4>
-                    <p>หมอหมึกช่วยผมเรื่องฤกษ์ยามใน การเจรจาธุรกิจได้มากเลย ตอนนี้ขายดีเทน้ําเทท่าเลยครับ</p><br/>
+                    <p>หมอหมึกช่วยผมเรื่องฤกษ์ยามใน การเจรจาธุรกิจได้มากเลย ตอนนี้ขายดีเทน้ําเทท่าเลยครับ</p>
                 </div>
-                <div class="row align-items-center py-3">
+                <div class="row">
                 <center>
-                        <button class="btn btn-primary button">
-                        <span class="spinner-border spinner-border-sm"></span>
-                            โปรดติดตาม
+                        <br/><button class="btn btn-light button">
+                        <span>
+                            อ่านต่อ
+                        </span>
                         </button>
                     </center>
                 </div>
             </div>
             <div class="column-3 align-items-center py-3">
-            <div classs="row">
+                <div classs="row SetHeight align-items-center">
                 <img src={jack} alt="Jack" className="roundImage"/><br/><br/>
                 <h4>แจ็ค นักแสดง</h4>
-                <p>ต้องขอบคุณหมอหมึกในการช่วยให้ความ สัมพันธ์ราบรื่นด้วยนะคะ ทํานายอะไรไว้ก็แม่นทุกอย่างเลยค่ะ</p><br/>
+                <p>ต้องขอบคุณหมอหมึกในการช่วยให้ความ สัมพันธ์ราบรื่นด้วยนะคะ ทํานายอะไรไว้ก็แม่นทุกอย่างเลยค่ะ</p>
                 </div>
-                <div class="row align-items-center">
-                    <center>
-                        <button class="btn btn-primary button">
-                        <span class="spinner-border spinner-border-sm"></span>
-                            โปรดติดตาม
+                <div class="row">
+                <center>
+                        <br/><button class="btn btn-light button">
+                        <span>
+                            อ่านต่อ
+                        </span>
                         </button>
                     </center>
                 </div>
             </div>
-            <div class="column-3 align-items-center">
-            <div classs="row align-items-center">
+            <div class="column-3 align-items-center py-3">
+            <div classs="row align-items-center SetHeight">
                 <img src={fahsai} alt="Fahsai" className="roundImage"/><br/><br/>
                 <h4>น้ําใส นักศึกษา</h4>
-                <p>ที่หมอหมึกบอกมานะคะเกิดเกือบหมดเลยค่ะ ถ้าไม่ได้หมอหมึกช่วยไว้ ชีวิตคงแย่เลยค่ะ ดีนะที่ป้องกันเรื่องร้าย ๆ ทันเวลา</p><br/>
+                <p>ที่หมอหมึกบอกมานะคะเกิดเกือบหมดเลยค่ะ ถ้าไม่ได้หมอหมึกช่วยไว้ ชีวิตคงแย่เลยค่ะ ดีนะที่ป้องกันเรื่องร้าย ๆ ทันเวลา</p>
                 </div>
                 <div class="row">
                 <center>
-                        <br/><button class="btn btn-primary button">
-                        <span class="spinner-border spinner-border-sm"></span>
-                            โปรดติดตาม
+                        <br/><button class="btn btn-light button">
+                        <span>
+                            อ่านต่อ
+                        </span>
                         </button>
                     </center>
                 </div>
--- src/components/form.js
+++ src/components/form.js
@@ -3,22 +3,31 @@ import './style.css';
 function Infom(){
     return(
         <>
+
+        <script>
+        </script>
+
         <div class="container py-5">
         <form>
         <div class="mb-3">
-            <label class="form-label">วันที่</label>
-            <input type="date" class="form-control" id="date"/>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">เวลา</label>
-            <input type="time" class="form-control" id="time"/>
-        </div>
-        <div class="mb-3">
             <label class="form-label">ชื่อ</label>
-            <input type="text" class="form-control" id="name"/>
+            <input type="text" class="form-control" id="name" required/>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">วันที่ต้องการจอง</label>
+            <input type="date" class="form-control" id="date" required/>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">เวลาที่ต้องการจอง</label>
+            <input type="time" class="form-control" id="time" required/>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Email ที่ติดต่อได้</label>
+            <input type="email" class="form-control" id="email" required/>
         </div>
         <center>
             <br/><button type="submit" class="btn btn-primary button">Submit</button>
+            <br/><br/><p>ขอบคุณสำหรับการจองของคุณ เราจะติดต่อกลับไปทางอีเมลเพื่อยืนยันการลงทะเบียน</p>
         </center>
         </form>
         </div>
